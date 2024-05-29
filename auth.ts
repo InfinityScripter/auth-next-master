36f846c34e02eb1af5bf8bdcbee879285fc06bdf
InfinityScripter <talalaev.misha@gmail.com>
2024-05-29 15:56:44 +0300

    add id user  -> identify session , add commit signIn check

--- auth.ts
+++ auth.ts
@@ -1,17 +1,36 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
+import {getUserById} from "@/data/user";
 import {db} from "@/lib/db"
 import authConfig from "./auth.config"
+import {UserRole} from ".prisma/client";
+
+
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     callbacks: {
+        // async signIn({user}) {
+        //     const existingUser = await getUserById(user.id)
+        //     if (!existingUser || existingUser.emailVerified) return false
+        //     return true
+        // },
         async session({session, token}) {
+            console.log("session", session, "SessionToken!:", token)
             if (token.sub && session.user) {
             session.user.id = token.sub}
+
+            if (token.role && session.user) {
+                session.user.role = token.role as UserRole
+            }
+
             return session
         },
         async jwt({token}) {
-            token.custom = 'custom'
+            if (!token.sub) return token
+const existingUser = await getUserById(token.sub);
+if (!existingUser) return token;
+
+            token.role = existingUser.role;
             return token
         }
     },
