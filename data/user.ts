1473539d17a3fe92aec5a1b67458aec926ac2c9e
InfinityScripter <talalaev.misha@gmail.com>
2024-05-29 15:54:40 +0300

    fix type for getUserById id

--- data/user.ts
+++ data/user.ts
@@ -9,11 +9,11 @@ export const getUserByEmail = async (email: string) => {
   }
 };
 
-export const getUserById = async (id: string) => {
-    try {
-        const user = await db.user.findUnique({ where: { id } });
-        return user;
-    } catch {
-        return null;
-    }
+export const getUserById = async (id: string | undefined) => {
+  try {
+    const user = await db.user.findUnique({ where: { id } });
+    return user;
+  } catch {
+    return null;
+  }
 };
