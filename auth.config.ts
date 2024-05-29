7758d390035a26bbbe7f643b912e40d33c8f0b27
InfinityScripter <talalaev.misha@gmail.com>
2024-05-29 16:29:20 +0300

    add sigIn Google , GitHub

--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,6 @@
 import Credentials from "next-auth/providers/credentials"
+import GitHub from "next-auth/providers/github"
+import Google from "@auth/core/providers/google";
 
 import type { NextAuthConfig } from "next-auth"
 
@@ -7,6 +9,14 @@ import {getUserByEmail} from "@/data/user";
 import bcrypt from "bcryptjs";
 
 export default { providers: [
+    Google({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    GitHub({
+        clientId: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
     Credentials({
         async authorize(credentials) {
             const validatedFields =  LoginSchema.safeParse(credentials);
--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,24 +1,29 @@
 'use client';
 
 import React from 'react';
+import {signIn} from "next-auth/react";
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
 import { Button } from '@/components/ui/button';
+import {DEFAULT_LOGIN_REDIRECT} from "@/routes";
 let count = 0
 
 const Social = () => {
+    const onClick = (provider:"google"|"github") => {
+        signIn(provider,{callbackUrl:DEFAULT_LOGIN_REDIRECT,})
+    }
   return (
     <div className='flex items-center space-x-4'>
       <Button variant='outline' size='lg' className='w-full' onClick={() => {
-          count +=1
-          localStorage.setItem('google','this google btn clicked')
-          alert(`this google btn clicked , ${count}`)
+   onClick("google")
 
       }}>
         <FcGoogle className='h-5 w-5' />
       </Button>
 
-      <Button variant='outline' size='lg' className='w-full' onClick={() => {}}>
+      <Button variant='outline' size='lg' className='w-full' onClick={() => {
+    onClick("github")
+      }}>
         <FaGithub className='h-5 w-5' />
       </Button>
     </div>
