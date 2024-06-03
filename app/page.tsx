8a0d1bf553175f36630bbb96d3773c7471978e39
InfinityScripter <talalaev.misha@gmail.com>
2024-06-03 12:21:06 +0300

    add some fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Poppins } from 'next/font/google';
 import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
 import { LoginButton } from '@/components/auth/login-button';
+import Image from "next/image";
 
 const font = Poppins({
   subsets: ['latin'],
@@ -9,30 +10,41 @@ const font = Poppins({
 });
 export default function Home() {
   return (
-    <>
-      <main className='flex h-full flex-col items-center justify-center bg-blue-500'>
-        <div className='space-y-6'>
-          <h1
-            className={cn(
-              'text-6xl font-semibold text-white drop-shadow-md',
-              font.className
-            )}
-          >
-            Auth
-          </h1>
-          <p className='text-left text-lg text-white'>
-            A simple authentication app built with Next.js, Tailwind CSS, and
-            Firebase.
-          </p>
-          <div>
-            <LoginButton mode='modal' asChild>
-              <Button variant='secondary' size='lg'>
-                Sign In
-              </Button>
-            </LoginButton>
+      <>
+        <main className='flex min-h-full h-full flex-col items-center justify-center bg-blue-500 py-12 sm:px-6 lg:px-8'>
+          <div className='space-y-6'>
+            <h1
+                className={cn(
+                    'text-4xl font-bold text-white',
+                    font,
+                    'text-center'
+                )}
+            >
+              My authenticating app
+            </h1>
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+              <div className="flex items-center justify-center">
+                <Image src={"/images/logo.png"} alt="logo" width={48} height={48} />
+              </div>
+            </div>
+            <p className='text-left text-lg text-white'>
+              A authenticating app built with Next.js, Tailwind CSS.
+            </p>
+            <div
+                className="
+                        flex
+                        items-center
+                        justify-center
+                        space-x-4"
+            >
+              <LoginButton mode='modal' asChild>
+                <Button variant='secondary' size='lg'>
+                  Sign In
+                </Button>
+              </LoginButton>
+            </div>
           </div>
-        </div>
-      </main>
-    </>
+        </main>
+      </>
   );
 }
